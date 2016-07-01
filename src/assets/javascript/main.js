20cdef8be8e9760b5652375805c9be08a19d5e02
Ryan Aponte <ryan@ryanaponte.com>
2016-07-01 13:33:04 -0400

    remove opacity styles, empty main.js, no import of style partials

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -3,44 +3,25 @@
 (function() {
 
   // visual grid: use 'g' key to toggle body class
-  var k = new Kibo();
-  k.down(['g'], function() {
-    $('body').addClass('grid');
-  }).up('g', function() {
-    $('body').removeClass('grid');
-  });
+  // var k = new Kibo();
+  // k.down(['g'], function() {
+  //   $('body').addClass('grid');
+  // }).up('g', function() {
+  //   $('body').removeClass('grid');
+  // });
 
   // navigation
-  $(window).resize(function() {
-    var more = document.getElementById("js-navigation-more");
-    if ($(more).length > 0) {
-      var windowWidth = $(window).width();
-      var moreLeftSideToPageLeftSide = $(more).offset().left;
-      var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+  // var menuToggle = $("#js-mobile-menu").unbind();
+  // $("#js-navigation-menu").removeClass("show");
 
-      if (moreLeftSideToPageRightSide < 330) {
-        $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
-        $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
-      }
-
-      if (moreLeftSideToPageRightSide > 330) {
-        $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
-        $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
-      }
-    }
-  });
-
-    var menuToggle = $("#js-mobile-menu").unbind();
-    $("#js-navigation-menu").removeClass("show");
-
-    menuToggle.on("click", function(e) {
-      e.preventDefault();
-      $("#js-navigation-menu").slideToggle(function(){
-        if($("#js-navigation-menu").is(":hidden")) {
-          $("#js-navigation-menu").removeAttr("style");
-        }
-      });
-    });
+  // menuToggle.on("click", function(e) {
+  //   e.preventDefault();
+  //   $("#js-navigation-menu").slideToggle(function(){
+  //     if($("#js-navigation-menu").is(":hidden")) {
+  //       $("#js-navigation-menu").removeAttr("style");
+  //     }
+  //   });
+  // });
 
 
 })();
