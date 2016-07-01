234db7c8e06d2e2e6912ec0ff1d20d20039cc3cb
Ryan Aponte <ryan@ryanaponte.com>
2016-07-01 17:24:59 -0400

    layout js working on homepage

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -6,6 +6,11 @@
 
   // this function contains others that need to run on every smoothsState page change
   function prep(){
+
+    //
+    // 1 these run at every window width
+    //
+
     // visual grid
     var k = new Kibo();
     k.down(['g'], function() {
@@ -27,13 +32,81 @@
       });
     });
 
+    //
+    //  2 - these run in an enquire conditional
+    //
+
+    // mobile
+    enquire.register("screen and (max-width: 768px)", {
+        match : function() {
+
+          $(function() {
+            // on load
+            // console.log('up to 768px wide');
+
+            // on resize
+          });
+        },
+        unmatch : function() {
+        }
+      })
+      // desktop
+      .register("screen and (min-width: 769px)", {
+        match : function() {
+          var windowWidth = $(window).width(); // for resize below
+          $(function() {
+            //
+            // on load
+            //
+
+            // homepage
+            $('#home-favorites').not('.processed').imagesLoaded( function() {
+              var favImage = $('#favImage');
+              var favImageHeight = favImage.height();
+              var favText = $('.favorites .wrapper');
+              var makerImage = $('#makerImage');
+              var makerImageHeight = makerImage.height();
+              var makerText = $('.winemaker .wrapper');
+              $(this).addClass('processed');
+
+              favImage.parent().prev().height(favImageHeight);
+              makerImage.parent().next().height(makerImageHeight);
+
+              setTimeout(function(){
+                favText.fadeTo(400, 1);
+                makerText.fadeTo(400, 1);
+              }, 100);
+            });
+
+            //
+            // on resize
+            //
+
+            // homepage
+            window.onresize = debounce(function(){
+              if ($(window).width() != windowWidth) {
+                var favImage = $('#favImage');
+                var favImageHeight = favImage.height();
+                var makerImage = $('#makerImage');
+                var makerImageHeight = makerImage.height();
+                favImage.parent().prev().height(favImageHeight);
+                makerImage.parent().next().height(makerImageHeight);
+              }
+            }, 100);
+
+          });
+        },
+        unmatch : function() {
+        }
+      });
+
 
 
   } // end of prep()
 
 
 
-
+  // smoothstate
   var $page = $('#main'),
     options = {
       blacklist: '.no-smoothState',
@@ -64,6 +137,21 @@
     },
     smoothState = $page.smoothState(options).data('smoothState');
 
-
+    // debounce
+    // https://davidwalsh.name/javascript-debounce-function
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
 
 })();
